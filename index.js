52011c15edab9c57b24b7981339df352381ab0f3
Mursaleen Taqi <leemursa@gmail.com>
2019-12-11 11:49:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
   
 const var1 = 31
 const var2 = 2
-const multiply = var1 * var2
+const multiply = 31 * 2
 const random = Math.floor( Math.random() * 1000 ) + 1
 const mod = 16 % 6
 const max = Math.max( 5, 10, 15, 20 )
